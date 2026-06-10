c7fbb35919fba5ba5b050e4e11a1e77883217d4b
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-06-10 15:00:38 +0530

    add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,36 @@
+import { NavLink } from "react-router-dom";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const pages = [
+    { path: "/", label: "Home" },
+    { path: "/subjects", label: "Subjects" },
+    { path: "/result", label: "Leaderboard" },
+  ];
+
+  return (
+    <div className="flex items-center justify-between p-5 w-full h-16 px-10">
+      <div className="flex items-center text-2xl font-bold">
+        <p className="text-[#FF6B00]">Bright</p>
+        <p className="text-[#00BCD4]">Roots</p>
+      </div>
+      <div className="flex items-center gap-8">
+        {pages.map((link) => {
+          return (
+            <NavLink
+              to={link.path}
+              className={({ isActive }) =>
+                isActive
+                  ? "text-md text-[#FF6B00] hover:bg-orange-100 rounded-lg px-2 py-1 transition-colors duration-200"
+                  : "text-md text-[#888888] hover:bg-zinc-200 rounded-lg px-2 py-1 transition-colors duration-200"
+              }
+            >
+              {link.label}
+            </NavLink>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
