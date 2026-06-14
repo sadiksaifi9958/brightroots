a4f0b3e4cac80eb37815222fe02471e50d7fe0ed
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-06-14 17:15:35 +0530

    add bottom margin in option container

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -176,7 +176,7 @@ function Quiz() {
             {firstQuestion.question}
           </h1>
         </div>
-        <div className="flex flex-col gap-4 w-full sm:w-70/100">
+        <div className="flex flex-col gap-4 w-full sm:w-70/100 mb-4">
           {shuffledOptions.map((option, index) => (
             <button
               className={`sm:px-6 sm:py-6 px-4 py-4 border rounded-2xl flex items-center text-left gap-4 cursor-pointer active:translate-y-1 transition-transform duration-150 ${option === firstQuestion.answer && selectedOption ? "bg-[#E0F7FA] border-[#00BCD4] text-[#00838F]" : option === selectedOption && option !== firstQuestion.answer ? "bg-[#FFEBEE] border-[#E57373] text-[#C62828]" : "bg-[#FFF3E8] border-[#FFD9B3] text-[#1A1A1A]"} ${
