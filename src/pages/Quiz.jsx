5f9b0ba2835cb55c97848f56cd14e6f229c90f4d
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-06-14 15:53:34 +0530

    make quiz page responsive and fix some responsive issue in Subjects page

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { RxCross1 } from "react-icons/rx";
+import { BsArrowLeft } from "react-icons/bs";
 import { FaRegClock } from "react-icons/fa";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { FaArrowRight } from "react-icons/fa";
@@ -123,12 +123,12 @@ function Quiz() {
 
   return (
     <div className="w-full">
-      <div className="w-full py-10 flex justify-between px-4">
+      <div className="w-full py-5 sm:py-8 flex justify-between px-4">
         <button
           className="p-4 text-[#888888] text-lg border border-[#FFD9B3] rounded-xl hover:not-only:bg-white w-fit cursor-pointer active:scale-95 transition-all duration-200"
           onClick={() => navigate("/subjects")}
         >
-          <RxCross1 />
+          <BsArrowLeft />
         </button>
         <div className="text-[#1A1A1A] font-bold">{displayName}</div>
         <div className="px-4 h-8 flex items-center font-bold text-[#FF6B00] border border-[#FFD9B3] rounded-full text-center">
@@ -145,18 +145,20 @@ function Quiz() {
           ></div>
         </div>
       </div>
-      <div className="w-full flex flex-col items-center">
-        <div className="flex justify-between items-center py-6 w-70/100">
+      <div className="w-full flex flex-col items-center px-4 sm:px-0">
+        <div className="flex justify-between items-center py-6 w-full sm:w-70/100">
           <div className="text-[#FFD9B3] text-sm">{`Question ${currentQuestionIndex + 1} of 10`}</div>
           <div className="text-[#FFD9B3] text-sm">{`${((currentQuestionIndex + 1) / questions.length) * 100}% done`}</div>
         </div>
-        <div className="px-12 py-10 border border-[#FFD9B3] rounded-2xl flex flex-col gap-4 items-center mb-4 w-70/100 bg-[#FFF3E8]">
+        <div className="px-4 py-6 sm:px-12 sm:py-10 border border-[#FFD9B3] rounded-2xl flex flex-col gap-4 items-center mb-4 w-full sm:w-70/100 bg-[#FFF3E8]">
           <div className="flex gap-3 items-center justify-center">
             <div className="text-[#FF6B00] text-lg">
               <FaRegClock />
             </div>
             <div className="text-[#FF6B00] text-xl">Remaining Time:</div>
-            <div className="text-[#FF6B00] text-4xl font-bold">{timeLeft}s</div>
+            <div className="text-[#FF6B00] text-2xl sm:text-4xl font-bold">
+              {timeLeft}s
+            </div>
           </div>
 
           <div className="w-full bg-[#FFD9B3] h-3 rounded-full">
@@ -166,18 +168,18 @@ function Quiz() {
             ></div>
           </div>
         </div>
-        <div className="py-6 px-8 bg-[#FF6B00] rounded-2xl mb-4 w-70/100">
+        <div className="sm:py-6 sm:px-8 px-4 py-4 bg-[#FF6B00] rounded-2xl mb-4 w-full sm:w-70/100">
           <div className="text-[#FFD9B3] text-sm uppercase mb-1">
             Question 3
           </div>
-          <h1 className="text-[#FFF8F2] text-2xl font-bold">
+          <h1 className="text-[#FFF8F2] sm:text-2xl text-lg  font-bold">
             {firstQuestion.question}
           </h1>
         </div>
-        <div className="flex flex-col gap-4 w-70/100">
+        <div className="flex flex-col gap-4 w-full sm:w-70/100">
           {shuffledOptions.map((option, index) => (
             <button
-              className={`px-6 py-6 border rounded-2xl flex items-center gap-4 mb-4 cursor-pointer active:translate-y-1 transition-transform duration-150 ${option === firstQuestion.answer && selectedOption ? "bg-[#E0F7FA] border-[#00BCD4] text-[#00838F]" : option === selectedOption && option !== firstQuestion.answer ? "bg-[#FFEBEE] border-[#E57373] text-[#C62828]" : "bg-[#FFF3E8] border-[#FFD9B3] text-[#1A1A1A]"} ${
+              className={`sm:px-6 sm:py-6 px-4 py-4 border rounded-2xl flex items-center gap-4 mb-4 cursor-pointer active:translate-y-1 transition-transform duration-150 ${option === firstQuestion.answer && selectedOption ? "bg-[#E0F7FA] border-[#00BCD4] text-[#00838F]" : option === selectedOption && option !== firstQuestion.answer ? "bg-[#FFEBEE] border-[#E57373] text-[#C62828]" : "bg-[#FFF3E8] border-[#FFD9B3] text-[#1A1A1A]"} ${
                 selectedOption &&
                 option !== firstQuestion.answer &&
                 option !== selectedOption
@@ -195,7 +197,7 @@ function Quiz() {
           ))}
         </div>
         {selectedOption && (
-          <div className="w-70/100 bg-sky-100 border border-sky-200 rounded-xl px-6 py-4 flex flex-col gap-2 mb-4">
+          <div className="sm:w-70/100 w-full bg-sky-100 border border-sky-200 rounded-xl px-6 py-4 flex flex-col gap-2 mb-4">
             <div className="flex gap-4 text-[#00BCD4] items-center">
               <div className="text-xl">
                 <HiOutlineLightBulb />
@@ -209,7 +211,7 @@ function Quiz() {
         )}
         {selectedOption && (
           <button
-            className="py-3 border border-[#00BCD4] text-[#1A1A1A] text-lg w-70/100 rounded-xl mb-6 cursor-pointer font-bold flex gap-3 justify-center items-center active:scale-95 active:bg-[#00BCD4]/20 transition-all duration-150"
+            className="py-3 border border-[#00BCD4] text-[#1A1A1A] text-lg sm:w-70/100 w-full rounded-xl mb-6 cursor-pointer font-bold flex gap-3 justify-center items-center active:scale-95 active:bg-[#00BCD4]/20 transition-all duration-150"
             onClick={handleNext}
           >
             Next Question <FaArrowRight />
