caadde88beec21f883cc416c74de6530ae3b9a76
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-06-14 13:59:26 +0530

    Update Navbar.jsx
    make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,20 +8,20 @@ function Navbar() {
   ];
 
   return (
-    <div className="flex items-center justify-between w-full h-16 px-10 bg-white">
-      <div className="flex items-center text-2xl font-bold">
+    <div className="flex items-center justify-between w-full h-16 px-4 sm:px-10 bg-white">
+      <div className="flex items-center sm:text-2xl text-xl shrink-0 font-bold">
         <span className="text-[#FF6B00]">Bright</span>
         <span className="text-[#00BCD4]">Roots</span>
       </div>
-      <nav className="flex items-center gap-8">
+      <nav className="flex items-center sm:gap-8 gap-1">
         {pages.map((link) => {
           return (
             <NavLink
               to={link.path}
               className={({ isActive }) =>
                 isActive
-                  ? "text-md text-[#FF6B00] hover:bg-orange-100 rounded-lg px-2 py-1 transition-colors duration-200"
-                  : "text-md text-[#888888] hover:bg-zinc-200 rounded-lg px-2 py-1 transition-colors duration-200"
+                  ? "text-sm sm:text-md text-[#FF6B00] hover:bg-orange-100 rounded-lg px-2 py-1 transition-colors duration-200"
+                  : "text-sm sm:text-md text-[#888888] hover:bg-zinc-200 rounded-lg px-2 py-1 transition-colors duration-200 whitespace-nowrap"
               }
             >
               {link.label}
