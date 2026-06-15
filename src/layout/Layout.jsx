1c7e51378b1cada8df403b6059aae1e12bac3abb
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-06-15 20:58:41 +0530

    add scroll effect in every page

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop";
 
 function Layout() {
   return (
     <div className="bg-[#FFF8F2] w-full">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
